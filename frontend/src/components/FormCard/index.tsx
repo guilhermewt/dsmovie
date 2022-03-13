1a0c0fce697ee12d5cbb11c04c12fc320429c427
welliston <welliston10@gmail.com>
2022-03-13 14:19:38 -0300

    pagination

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -16,7 +16,7 @@ function FormCard({ movieId }: Props) {
     useEffect(() => {
         axios.get(`${BASE_URL}/movies/${movieId}`).then(response => {
             setMovie(response.data);
-        })
+        });
     }, [movieId]);
    
 
--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -6,15 +6,15 @@ type Props = {
     count: number;
 }
 
-
-function MovieScore({score, count} : Props) {
+function MovieScore({ score, count } : Props) {
 
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars score={score}/>
+            <MovieStars score={score} />
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     );
 }
+
 export default MovieScore;
